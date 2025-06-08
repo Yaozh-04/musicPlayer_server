d03fa52f63889394152fc769387e1c07277d2654
Y-xiaoyao-Y <2361225110@qq.com>
2025-06-08 11:06:34 +0800

    1.0

---
+++ src/main/java/com/yao/musicplayer_server/MusicPlayerServerApplication.java
@@ -0,0 +1,13 @@
+package com.yao.musicplayer_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicPlayerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicPlayerServerApplication.class, args);
+    }
+
+}
