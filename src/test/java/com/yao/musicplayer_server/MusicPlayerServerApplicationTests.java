ea29e39670a26398002f023df83760400579087c
Y-xiaoyao-Y <2361225110@qq.com>
2025-06-08 17:53:01 +0800

    2.0

---
+++ src/test/java/com/yao/musicplayer_server/MusicPlayerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yao.musicplayer_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicPlayerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
